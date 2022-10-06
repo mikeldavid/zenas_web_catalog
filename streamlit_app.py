57c992e440e9002ebb1063bfbfaddb698a32b97c
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-10-06 16:14:11 +1100

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,9 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("select color_or_style from catalog_for_website")
+#my_catalog = my_cur.fetchall()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
